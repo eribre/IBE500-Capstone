904860ab59566513f97314790bceb141ad475ecd
Erik Brendehaug <70907050+eribre@users.noreply.github.com>
2024-04-03 19:32:55 +0200

    Working on fix for getTx and viewing multiple tx

--- scripts/getTx.js
+++ scripts/getTx.js
@@ -1,71 +1,45 @@
 /* eslint-disable global-require */
 /* eslint-disable consistent-return */
+
+const { get } = require("../routes");
+
 /* eslint-disable no-undef */
 process.env.NETWORK = "testnet";
 
 async function getTx(id) {
-	const txId = id;
+    const txId = id;
 
-	const WhatsApi = require("whatsonchain/index");
+    const woc = require("whatsonchain");
 
-	console.log("Executing on the", process.env.NETWORK);
+    /**
+     * Receives the transaction Id and the output index
+     * Retrieves the message stored in that output
+     * If there is no message, it raises an error
+     * @param {string} txId
+     * @param {number} outputIndex
+     * @return {Promise<string>}
+     */
+    const getMessage = async function (txId, outputIndex) {
+        const whatsonchain = new woc("test");
+        let result = await whatsonchain.getRawTxOutputData(txId, outputIndex);
+        if (result.startsWith("006a")) {
+            return Buffer.from(result.substring(4), "hex").toString();
+        } else {
+            return Promise.reject("Failure: no message in this output");
+        }
+    };
 
-	// eslint-disable-next-line func-names
-	async function accessChain(param) {
-		console.log("Contacting WhatsOnChain...");
-		const woc = new WhatsApi("testnet");
-		try {
-			return await woc.txHash(param);
-			// let stat = await woc.status();
-			// let tx = await woc.txHash("fac4a416140760fa851a24abc857880209fded46e04a3a16bcd9342bb378d987");
-		} catch (e) {
-			console.log(e);
-		}
-	}
-
-	async function txOutput() {
-		/*
-		const output = await accessChain(txId).then((result) => {
-			const fLen = result.vout.length;
-			for (let i = 0; i < fLen; i += 1) {
-				const scriptHex = result.vout[i].scriptPubKey.hex;
-				const isData = scriptHex.startsWith("006a");
-				if (isData) {
-					const data = Buffer.from(
-						scriptHex.substring(4),
-						"hex"
-					).toString();
-					return data;
-				}
-			}
-			return data;
-		});
-		return output; */
-
-		const result = await accessChain(txId);
-		const fLen = result.vout.length;
-
-		for (let i = 0; i < fLen; i += 1) {
-			const scriptHex = result.vout[i].scriptPubKey.hex;
-			const isData = scriptHex.startsWith("006a");
-			if (isData) {
-				const data = Buffer.from(
-					scriptHex.substring(4),
-					"hex"
-				).toString();
-				return data;
-			}
-		}
-		return data;
-	}
-
-	const outputToWeb = await txOutput();
-	console.log(outputToWeb);
-	return outputToWeb;
+    const outputToWeb = await getMessage(txId, 0);
+    console.log(outputToWeb);
+    return outputToWeb;
 }
 
 // const output = getTx(
 //	"da7f03b60031622097049245365be31cf02e729fafedd94099f6f0e7bf178e33"
 // );
+
+//console.log(
+//    getTx("a2424f81dd138cd899994768fed5c80a130fa8ee0917d3d1c5ab75d3b9b16027"),
+//);
 
 module.exports = getTx;
